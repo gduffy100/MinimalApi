8ae4153bf08556d31c0245de1e23f593dcf07591
gduffy100 <gduffy100@hotmail.com>
2022-02-12 15:48:01 +0000

    RRW-1-feature: car and controller

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MalfunctionChance { get; set; }
+        public int DistanceCoveredInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+
+    }
+}
---
+++ WebUI/Features/Cars/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Domain.Entities;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/cars")] // "api/[controller]" dynamic binding less safe than strongly typed
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> getCars()
+        {
+            var cars = new List<Car>();
+            var car1 = new Car
+            {
+                TeamName = "team A",
+                Speed = 100,
+                MalfunctionChance = 0.2
+            };
+            var car2 = new Car
+            {
+                TeamName = "team B",
+                Speed = 90,
+                MalfunctionChance = 0.1
+            };
+            cars.Add(car1);
+            cars.Add(car2);
+
+            return Ok(cars);
+        }
+    }
+}
