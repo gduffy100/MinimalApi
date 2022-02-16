9ec15ae910fceafa15a16bf6da916eb918341929
gduffy100 <gduffy100@hotmail.com>
2022-02-16 21:52:53 +0000

    MMA-2-Feature: Car Persistence

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// add the entity framework core db context
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<RaceDb>(options =>
+options.UseSqlServer(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,47 +27,88 @@ app.UseHttpsRedirection();
 #region cars endpoints
 // cars endpoints
 
-app.MapGet("api/cars", () => { }
+app.MapGet("api/cars", (RaceDb db) => {
+
+    var cars = db.Cars.ToList();
+
+    return Results.Ok(cars);
+}
 
 
 ).WithName("GetCars");
 
-app.MapGet("api/cars/{id}", (int id) =>{ 
+app.MapGet("api/cars/{id}", (int id, RaceDb db) =>{
+    
+    var car = db.Cars.FirstOrDefault(x => x.Id == id);
+
+    if (car == null)
+    {
+        return Results.NotFound($"Car with id:{id} not found");
+    }
+    return Results.Ok(car);
 
 }).WithName("GetCar");
 
-app.MapPost("api/cars", (Car car) => {
 
-    return car;
-}).WithName("CreateCar");
+app.MapPut("api/cars/{id}", (int id, RaceDb db, CarUpdateModel carModel) => {
 
-app.MapPut("api/cars/{id}", (int id) => {
+    var dbCar = db.Cars.FirstOrDefault(x => x.Id == carModel.Id);
 
-    return id;
+    if (dbCar == null)
+    {
+        return Results.NotFound($"Car with id:{carModel.Id} not found");
+    }
+
+    dbCar.TeamName = carModel.TeamName;
+    dbCar.Speed = carModel.Speed;
+    dbCar.MalfunctionChance = carModel.MalfunctionChance;
+
+    db.SaveChanges();
+
+    return Results.Ok(dbCar);
+
 }).WithName("UpdateCar");
 
-app.MapPost("api/cars", (Car car) => {
+app.MapPost("api/cars", (CarCreateModel carModel, RaceDb db) => {
 
-    return car;
-}).WithName("CreateCar");
+    var car = new Car
+    {
+        TeamName = carModel.TeamName,
+        Speed = carModel.Speed,
+        MalfunctionChance = carModel.MalfunctionChance
+    };
+    db.Cars.Add(car);
+    db.SaveChanges();
 
-app.MapDelete("api/cars/{id}", (int id) => {
+    return Results.Ok(car);
 
-    return $"car with id {id} has been deleted";
+ }).WithName("CreateCar");
+
+app.MapDelete("api/cars/{id}", (int id, RaceDb db) => {
+
+    var dbCar = db.Cars.FirstOrDefault(x => x.Id == id);
+
+    if (dbCar == null)
+    {
+        return Results.NotFound($"Car with id:{id} not found");
+    }
+    db.Remove(dbCar);
+    db.SaveChanges();
+
+    return Results.Ok($"Car with ID: {id} has been deleted");
 }).WithName("DeleteCar");
 
 #endregion
 
-//motorbike endpoints
+#region motorbike endpoints
 
-// default endpoint weather
-
+#endregion
 
 app.Run();
 
 
-// models
-internal record Car //does not take parameters // record is new c#9 concept
+#region models
+public record Car //does not take parameters // record is new c#9 concept
 {
     public int Id { get; set; }
     public string? TeamName { get; set; } // ? to declare as nullable
@@ -70,7 +119,23 @@ internal record Car //does not take parameters // record is new c#9 concept
     public int RacedForHours { get; set; }
 }
 
-internal record Motorbike //does not take parameters // record is new c#9 concept
+public record CarUpdateModel //does not take parameters // record is new c#9 concept
+{
+    public int Id { get; set; }
+    public string? TeamName { get; set; } // ? to declare as nullable
+    public int Speed { get; set; }
+    public double MalfunctionChance { get; set; }
+}
+
+public record CarCreateModel //does not take parameters // record is new c#9 concept
+{
+    public string? TeamName { get; set; } // ? to declare as nullable
+    public int Speed { get; set; }
+    public double MalfunctionChance { get; set; }
+}
+
+
+public record Motorbike //does not take parameters // record is new c#9 concept
 {
     public int Id { get; set; }
     public string? TeamName { get; set; } // ? to declare as nullable
@@ -80,3 +145,19 @@ internal record Motorbike //does not take parameters // record is new c#9 concep
     public int FinishedRace { get; set; }
     public int RacedForHours { get; set; }
 }
+
+#endregion
+
+#region persistence
+
+public class RaceDb : DbContext // must inherit from this to be used in db connection
+{
+    //need constructor 
+    public RaceDb(DbContextOptions<RaceDb> options) : base(options) // inherit base options
+        {
+    }
+    public DbSet<Car>? Cars { get; set; }
+    
+}
+
+#endregion
